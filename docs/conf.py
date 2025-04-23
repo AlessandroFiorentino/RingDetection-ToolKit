29d861b20da1ba32a453273744cf64e6b3dc2576
Alessandro Fiorentino <113921751+AlessandroFiorentino@users.noreply.github.com>
2025-04-23 16:28:21 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath('../RingDetectionToolKit'))
 project = 'RingDetection'
 copyright = '2025, Alessandro Fiorentino'
 author = 'Alessandro Fiorentino'
-release = '1.0.a0'
+release = '1.0.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
