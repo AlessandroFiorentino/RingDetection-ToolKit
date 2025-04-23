7e9c49fa42dad5ef6c3395d96e207a25cdda5a66
Alessandro Fiorentino <113921751+AlessandroFiorentino@users.noreply.github.com>
2025-04-23 17:08:00 +0200

    Update setup.py

--- RingDetectionToolkit/setup.py
+++ RingDetectionToolkit/setup.py
@@ -5,6 +5,13 @@ setup(
     version="1.0.0",
     packages=find_packages(),
     install_requires=[
-        # Add your dependencies here
+        "numpy>=1.26.0,<1.27.0",
+        "matplotlib==3.7.1",
+        "scikit-learn==1.2.2",
+        "scipy==1.10.1",
+        "tqdm==4.65.0",
+        "sphinx==7.2.6",
+        "sphinx_rtd_theme==1.3.0",
+        "ghp-import==2.1.0",
     ],
 )
