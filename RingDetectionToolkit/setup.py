aa213248ab74296ebb3f2ca22880da6ad5552b17
Alessandro Fiorentino <113921751+AlessandroFiorentino@users.noreply.github.com>
2025-04-23 16:36:56 +0200

    Create setup.py

---
+++ RingDetectionToolkit/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="RingDetectionToolkit",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=[
+        # Add your dependencies here
+    ],
+)
