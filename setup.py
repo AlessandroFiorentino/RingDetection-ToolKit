cd11d047e86182c601fa53116ab800079e2d8a2d
Alessandro Fiorentino <113921751+AlessandroFiorentino@users.noreply.github.com>
2025-04-23 16:28:58 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="RingDetectionToolkit",
-    version="0.1.0",
+    version="1.0.0",
     author="Alessandro Fiorentino",
     author_email="alexfiore98@gmail.com",
     description="A toolkit for ring detection tasks.",
