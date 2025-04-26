6bdebdea5cf6d2a6000377bda29330b247e54a04
Alessandro Fiorentino <113921751+AlessandroFiorentino@users.noreply.github.com>
2025-04-26 18:53:30 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -51,4 +51,4 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # Theme options
 html_theme = 'sphinx_rtd_theme'
-#html_static_path = ['_static']
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
